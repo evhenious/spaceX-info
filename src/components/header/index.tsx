f8c4ce1f3d1819171c166355b5552df823c054f6
evhenious <evhenious@gmail.com>
2020-01-28 21:39:58 +0200

    able to scroll title page into info

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,12 +14,18 @@ const Header = () => {
     return null;
   }
 
+  let headerRef: HTMLElement | null;
+
+  const scrollToList = () => {
+    window.scrollTo({ top: headerRef?.clientHeight, behavior: 'smooth' });
+  };
+
   return (
-    <header className='App-header'>
+    <header className='App-header' ref={ref => {headerRef = ref}}>
       <div className={'disclaimor'}>* All rights to logo and content belong to it's respectable owners. The page created just for fun and training purposes.</div>
       <img src={logo} className='App-logo' alt='logo' />
       {loading ? '' : <p className={'App-header-summary'} >{data.company.summary}</p>}
-      {loading ? '' : <img className={'App-scroll-sign'} src={arrowDown} alt={'scroll down'} />}
+      {loading ? '' : <img className={'App-scroll-sign'} src={arrowDown} alt={'scroll down'} onClick={scrollToList} />}
     </header>
   );
 };
