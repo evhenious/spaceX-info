64538dec239bdae390bb36fa5c2d0e19835ab2f2
Yevhen H <yevhen.hraivoronskyi@altexsoft.com>
2020-07-03 17:53:55 +0300

    timeline service header - begin

---
+++ src/components/timeline/ServiceHeader/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const ServiceHeader: React.FC<{}> = (props) => {
+  return <div className='service-header'>
+    header
+  </div>
+}
+
+export default ServiceHeader;
--- src/components/timeline/timelineMarkup.tsx
+++ src/components/timeline/timelineMarkup.tsx
@@ -4,12 +4,13 @@ import TimelineItem from './timelineItem';
 import moment from 'moment';
 
 import { iLaunch } from '../apollo/interface';
+import ServiceHeader from './ServiceHeader';
 
 interface Props {
   launches: iLaunch[];
 }
 
-const lookForDarkLogoScroll = (name: string, setClassName: Function) => {
+const getMainHeaderObserver = (name: string, setClassName: Function) => {
   let observer = new IntersectionObserver(([entry]) => {
     if (entry.intersectionRatio === 0) {
       setClassName('bg-scroll-fixed');
@@ -30,27 +31,31 @@ const TimelineMarkup: React.FC<Props> = (props) => {
 
   useEffect(() => {
     const element = document.querySelector(".App-header");
-    console.log(element)
     if (element && !observer) {
-      setObserver(lookForDarkLogoScroll(".App-header", setAdditionalClassName));
+      setObserver(getMainHeaderObserver(".App-header", setAdditionalClassName));
     }
   }, [observer]);
 
   return (
-    <div className={`timeline ${additionalClassName}`}>
-      { launches.map((item, index) => {
-        const { mission_name, details, launch_date_utc, id, launch_success } = item;
-        const date = moment(launch_date_utc).format('MMM Do YY');
-        return <TimelineItem
-          key={`${launch_date_utc}`}
-          title={`${date} - ${mission_name}`}
-          content={details}
-          launchID={id}
-          right={Boolean(index % 2)}
-          success={launch_success}
-        />
-      }) }
-    </div>
+    <>
+      <ServiceHeader  />
+      <div className={`timeline ${additionalClassName}`}>
+        {launches.map((item, index) => {
+          const { mission_name, details, launch_date_utc, id, launch_success } = item;
+          const date = moment(launch_date_utc).format('MMM Do YY');
+          return (
+            <TimelineItem
+              key={`${launch_date_utc}`}
+              title={`${date} - ${mission_name}`}
+              content={details}
+              launchID={id}
+              right={Boolean(index % 2)}
+              success={launch_success}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
